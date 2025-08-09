3dcf7fa67d2ad92ec09d7e5123e519d0206c01a4
ArcheryTechnocrats <techfiles.atpl@gmail.com>
2025-08-10 03:21:54 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    headers: {
-      "Content-Type": "text/css; charset=utf-8",
-    },
-  },
   build: {
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
+        manualChunks: undefined,
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
       },
+    },
+  },
+  server: {
+    headers: {
+      "Cache-Control": "no-cache",
     },
   },
 });
